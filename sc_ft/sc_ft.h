d043f7a41b93c36c03f3182b189b56edc13cc4d6
Tony Kirke <tkirke@broadcom.com>
2013-07-26 16:10:27 -0700

    Add sc_ft.h

---
+++ sc_ft/sc_ft.h
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////// 
+// Copyright© 2013 by Broadcom Corporation. All Rights reserved.
+// Origina Author : Tony Kirke, Broadcom Corporation.
+// The contents of this file are subject to the restrictions and limitations
+// set forth in the SystemC Open Source License Version 3.1 (the "License").
+// You may not use this file except in compliance with such restrictions and
+// limitations. You may obtain instructions on how to receive a copy of the
+// License at http://www.accellera.org/. Software distributed by Contributors
+// under the License is distributed exclusively on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied.
+// See the License for the specific language governing rights and limitations
+// under the License.
+////////////////////////////////////////////////////////////////////////////// 
+
+#ifndef SYSTEMC_SC_FT_ALL_H_
+#define SYSTEMC_SC_FT_ALL_H_
+
+#include <sc_fixed.h>
+#include <sc_ufixed.h>
+#include <sc_mixed_functions.h>
+
+#endif
